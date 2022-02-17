64a994ddb6ec7ec7210817e75147e361b8fccbf7
Rodrigo Rogovski <rfrogovski@gmail.com>
2022-02-17 16:29:14 -0400

    feat: crypto service pagination

--- src/service/CryptoService.js
+++ src/service/CryptoService.js
@@ -7,6 +7,17 @@ class CryptoService {
   }
   async *list() {
     // TODO: implementar generator que chama a repository fazendo a paginação
+    const localData = data => new Crypto(data);
+
+    let page = 0;
+    while (true) {
+      page++;
+      const response = await this.repository.list(page);
+      console.log('response => ', response);
+      const data = response.data;
+
+      yield data.map(localData);
+    }
   }
 }
 
