f29a3c96207b3b26b719673cfe59b73373376ebd
Rodrigo Rogovski <rfrogovski@gmail.com>
2022-02-24 01:27:52 -0400

    feat: User coercion to primitive

--- src/entity/User.js
+++ src/entity/User.js
@@ -3,6 +3,9 @@ class User {
     this.id = id;
   }
   // TODO: Alguma manipulação a fazer considerando que o objeto gerado será constantemente escrito em tela?
+  [Symbol.toPrimitive](coercionType) {
+    return coercionType !== 'string' ? new TypeError() : `${this.id}`;
+  }
 }
 
 export default User;
