918c9a87fb6d323162e81f60e9000a076873a495
Rodrigo Rogovski <rfrogovski@gmail.com>
2022-03-07 00:38:04 -0400

    test: CryptoService suite tests

--- test/CryptoService.test.js
+++ test/CryptoService.test.js
@@ -1,32 +1,49 @@
 import { describe } from 'mocha';
 import { expect } from 'chai';
+import sinon from 'sinon';
+import chalk from 'chalk';
 
 import CryptoService from '../src/service/CryptoService.js';
 import cryptos from './mocks/cryptos.js';
-import Sinon from 'sinon';
 import CryptoRepository from '../src/repository/CryptoRepository.js';
 
 const mock = cryptos;
-const service = new CryptoService({ repository: new CryptoRepository() });
-
-// const stub = Sinon.stub(service, service.list.name);
-// stub.resolves(mock);
-
-const generator = service.list();
 
 describe('CryptoService Suite Tests', () => {
-    it('Should call the repository list to page 1', async () => {
-        let { value, done } = await generator.next();
+    let service = {}, sandbox = {};
+
+    before(() => {
+        service = new CryptoService({ repository: new CryptoRepository() });
+    })
+
+    beforeEach(() => {
+        sandbox = sinon.createSandbox();
+    })
+
+    afterEach(() => {
+        sandbox.restore();
+    })
+
+    it('Should call the repository list', async () => {
+        let page = 1, limit = 5;
+        sandbox.stub(service, service.list.name).callsFake(async function* () {
+            // console.log('page, limit => ', page, limit)
+            const _value = mock.slice((page - 1) * limit, page * limit);
+
+            if (!_value.length) return [];
+
+            yield _value;
+        });
+
+        let value, done, resultList = [];
 
         while (!done) {
-            console.log('generator', value.length, done);
-            ({ value, done } = await generator.next());
+            ({ value, done } = await service.list().next());
+            page += 1;
+            resultList = resultList.concat(value);
+            // console.log(chalk.red('resultList => '), resultList);
         }
 
-        // console.log('generator', value.length, done);
-
-        // // console.log('User =>', String(user));
-
-        // expect(String(user)).to.be.deep.equal(userId)
+        expect(JSON.stringify(mock)).to.be.deep.equal(JSON.stringify(resultList))
     })
 })
